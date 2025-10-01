071a385068f9d88e27739628a0aeb1c05d1254ac
joakor89 <joacorf33@gmail.com>
2025-10-01 12:03:31 -0600

    NYCFlights13

---
+++ chap_03/DT.R
@@ -0,0 +1,17 @@
+# Data Transformation
+
+### Loading Libraries
+
+library(tidyverse)
+library(nycflights13)
+
+### NYCFlights13
+
+flights
+
+View(flights)
+
+print(flights, width = Inf)
+
+glimpse(flights)
+
