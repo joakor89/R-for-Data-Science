23e0a890a87d11219a284e7575299c5e50ae1e5e
joakor89 <joacorf33@gmail.com>
2025-10-02 14:52:52 -0600

    Case Study: Aggregates & Sample Size

--- chap_03/DT.R
+++ chap_03/DT.R
@@ -245,3 +245,25 @@ flights %>%
     .by = c(origin, dest)
   )
 
+## Case Study: Aggregates & Sample Size
+
+library(tidyverse)
+
+batters <- Lahman::Batting %>% 
+  group_by(playerID) %>% 
+  summarize(
+    performance = sum(H, na.rm = TRUE) / sum(AB, na.rm = TRUE),
+    n = sum(AB, na.rm = TRUE)
+  )
+
+batters %>% 
+  filter(n > 100) %>% 
+  ggplot(aes(x = n, y = performance)) +
+  geom_point(alpha = 1 / 10) +
+  geom_smooth(se = FALSE)
+
+batters %>% 
+  arrange(desc(performance))
+
+
+
