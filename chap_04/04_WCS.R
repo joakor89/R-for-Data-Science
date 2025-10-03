8d27236c4897494104b2017efdf3201519b4b8e7
joakor89 <joacorf33@gmail.com>
2025-10-03 07:51:11 -0600

    Spaces

--- chap_04/04_WCS.R
+++ chap_04/04_WCS.R
@@ -11,13 +11,30 @@ short_flights <- flights %>% filter(air_time < 60) # strive for this
 
 SHORTFLIGHTS <- flights %>% filter(air_time < 60) # and avoid this
 
+### Spaces
 
+z <- (a + b)^2 /d # strive for this
 
+z<-(a+b) ^ 2/d # and avoid this
 
+mean(x, na.rm = TRUE) # strive for this
 
+mean (x ,na.rm=TRUE) # and avoid this
 
+flights %>% 
+  mutate(
+    speed      = distance / air_time,
+    dep_hour   = dep_time %>% 100,
+    dep_minute = dep_time %>% 100
+  )
 
+### Pipes
 
+flights %>% 
+  filter(!is.na(arr_delay), !is.na(tailnum)) %>% 
+  count(dest) # strive for this
+
+flights %>% filter(!is.na(arr_delay), !is.na(tailnum)) %>% count(dest) # and avoid this
 
 
 
