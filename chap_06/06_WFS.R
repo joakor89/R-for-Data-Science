51cb9a658ab3fdb6cd94f0550fe665ca352babb2
joakor89 <joacorf33@gmail.com>
2025-10-11 14:58:26 -0600

    Running Code

--- chap_06/06_WFS.R
+++ chap_06/06_WFS.R
@@ -1,2 +1,19 @@
 # Workflow: Scripts & Projects
 
+### Running Code
+
+library(dplyr)
+
+library(nycflights13)
+
+not_cancelled <- flights %>% 
+  group_by(!is.na(dep_delay), !is.na(arr_delay))
+
+not_cancelled %>% 
+  group_by(year, month, day) %>% 
+  summarize(mean = mean(dep_delay))
+
+
+
+
+
