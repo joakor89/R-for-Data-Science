ff468119e618963ca9e79681b0c77ee0beb43a1a
joakor89 <joacorf33@gmail.com>
2025-10-13 09:00:31 -0600

    Other Arguments

--- chap_07/07_DI.R
+++ chap_07/07_DI.R
@@ -39,13 +39,40 @@ students <- students %>%
 
 students
 
+### Other Arguments
 
+read_csv(
+  "a,b,b
+  1,2,3
+  4,5,6"
+)
 
+read_csv(
+  "The first line of metadata
+  The second line of metadata
+  x,y,z
+  1,2,3",
+  skip = 2
+)
 
+read_csv(
+  "# A comment I want to skip
+  x,y,z
+  1,2,3",
+  comment = "#"
+)
 
+read_csv(
+  "1,2,3
+  4,5,6",
+  col_names = FALSE
+)
 
+read_csv(
+  "1,2,3
+  4,5,6",
+  col_names = c("x", "y", "z")
+)
 
-
-
-
+### 
 
