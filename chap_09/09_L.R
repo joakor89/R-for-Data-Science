b8b464e5879c4d6b5044f80a6994f18f3c9eec65
joakor89 <joacorf33@gmail.com>
2025-10-14 09:25:13 -0600

    Aesthetic Mappings

--- chap_09/09_L.R
+++ chap_09/09_L.R
@@ -2,5 +2,31 @@
 
 # Prerequisites
 
-##### library(tidyverse)
+library(tidyverse)
+
+### Aesthetic Mappings
+
+mpg
+
+ggplot(mpg, aes(x = displ, y = hwy, color = class)) +
+  geom_point()
+
+ggplot(mpg, aes(x = displ, y = hwy, shape = class)) +
+  geom_point()
+
+ggplot(mpg, aes(x = displ, y = hwy, size = class)) +
+  geom_point()
+
+ggplot(mpg, aes(x = displ, y = hwy, alpha = class)) +
+  geom_point()
+
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point(color = "royalblue")
+
+
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point(color = "royalblue", shape = 2)
+
+### 
+
 
