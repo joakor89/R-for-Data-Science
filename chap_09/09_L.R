4ccac2136755640c7f7ceb7f33510c431b58197f
joakor89 <joacorf33@gmail.com>
2025-10-15 08:09:41 -0600

    Geometric Objects

--- chap_09/09_L.R
+++ chap_09/09_L.R
@@ -27,6 +27,94 @@ ggplot(mpg, aes(x = displ, y = hwy)) +
 ggplot(mpg, aes(x = displ, y = hwy)) +
   geom_point(color = "royalblue", shape = 2)
 
-### 
+### Geometric Objects
+
+##### Left
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point()
+
+##### Right
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_smooth()
+
+##### Left
+ggplot(mpg, aes(x = displ, y = hwy, shape = drv)) +
+  geom_smooth()
+
+##### Right
+ggplot(mpg, aes(x = displ, y = hwy, linetype = drv)) +
+  geom_smooth()
+
+ggplot(mpg, aes(x = displ, y = hwy, color = drv)) +
+  geom_point() +
+  geom_smooth(aes(linetype = drv))
+
+##### Left 
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_smooth()
+
+##### Middle 
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_smooth(aes(group = drv))
+
+##### Right
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_smooth(aes(color = drv), show.legend = FALSE)
+
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point(aes(color = class)) +
+  geom_smooth()
+
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point() +
+  geom_point(
+    data = mpg %>% filter(class == "2seater"),
+    color = "red"
+  ) +
+  geom_point(
+    data = mpg %>% filter(class == "2seater"),
+    shape = "circle open", size = 3, color = "red"
+  )
+
+
+##### Left 
+ggplot(mpg, aes(x = hwy)) +
+  geom_histogram(binwidth = 2)
+
+##### Middle 
+ggplot(mpg, aes(x = hwy)) +
+  geom_density()
+
+##### Right
+ggplot(mpg, aes(x = hwy)) +
+  geom_boxplot()
+
+library(ggridges)
+
+ggplot(mpg, aes(x = hwy, y = drv, fill = drv, color = drv)) +
+  geom_density_ridges(alpha = 0.5, show.legend = FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
