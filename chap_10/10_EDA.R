680545556676a2100202b2fa2fcd64d8982e4d70
joakor89 <joacorf33@gmail.com>
2025-10-16 11:51:37 -0600

    Unusual Values

--- chap_10/10_EDA.R
+++ chap_10/10_EDA.R
@@ -17,20 +17,45 @@ smaller <- diamonds %>%
 ggplot(smaller, aes(x = carat)) +
   geom_histogram(binwidth = 0.01)
 
+### Unusual Values
+
+ggplot(diamonds, aes(x = y)) +
+  geom_histogram(binwidth = 0.5)
+
+ggplot(diamonds, aes(x = y)) +
+  geom_histogram(binwidth = 0.5) +
+  coord_cartesian(ylim = c(0, 50))
+
+unusual <- diamonds %>% 
+  filter(y < 3 | y > 20) %>% 
+  select(price, x, y, z) %>% 
+  arrange(y)
+
+unusual
+
+diamonds2 <-  diamonds %>% 
+  filter(between(y, 3, 20))
+
+diamonds2 <- diamonds %>% 
+  mutate(y = if_else(y < 3 | y > 20, NA, y))
+
+ggplot(diamonds2, aes(x = x, y = y)) +
+  geom_point()
+
+ggplot(diamonds2, aes(x = x, y = y)) +
+  geom_point(na.rm = TRUE)
+
+nycflights13::flights %>% 
+  mutate(
+    cancelled = is.na(dep_time),
+    sched_hour = sched_dep_time %/% 100,
+    sched_min = sched_dep_time %/% 100,
+    sched_dep_time = sched_hour + (sched_min / 60)
+  ) %>% 
+  ggplot(aes(x = sched_dep_time)) +
+  geom_freqpoly(aes(color = cancelled), binwidth = 1/4)
+
 ### 
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
