33e63076b9ff69a92b4651cab59f4d0af06dc161
joakor89 <joacorf33@gmail.com>
2025-10-17 11:30:05 -0600

    Annotations

--- chap_11/11_C.R
+++ chap_11/11_C.R
@@ -33,20 +33,74 @@ ggplot(df, aes(x, y)) +
     y = quote(sum(x[i] ^ 2, i == 1, n))
   )
 
-###
+### Annotations 
 
+label_info <- mpg %>% 
+  group_by(drv) %>% 
+  arrange(desc(displ)) %>% 
+  slice_head(n = 1) %>% 
+  mutate(
+    drive_type = case_when(
+      drv == "f" ~ "front-wheel drive",
+      drv == "r" ~ "rear-wheel drive",
+      drv == "4" ~ "4-wheel drive"
+    )
+  ) %>% 
+  select(displ, hwy, drv, drive_type)
 
+label_info
 
+ggplot(mpg, aes(x = displ, y = hwy, color = drv)) +
+  geom_point(alpha = 0.3) +
+  geom_smooth(se = TRUE) +
+  geom_text(
+    data = label_info,
+    aes(x = displ, y = hwy, label = drive_type),
+    fontface = "bold", size = 5, hjust = "right", vjust = "botton"
+  ) +
+  theme(lengend.position = "none")
 
+ggplot(mpg, aes(x = displ, y = hwy, color = drv)) +
+  geom_point(alpha = 0.3) +
+  geom_smooth(se = FALSE) +
+  geom_label_repel(
+    data = label_info,
+    aes(x = displ, y = hwy, label = drive_type),
+    fontface = "bold", size = 5, nudge_y = 2
+  ) +
+  theme(legend.position = "none")
 
+potential_outliers <- mpg %>% 
+  filter(hwy > 40 | (hwy > 20 & displ > 5))
 
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point() +
+  geom_text_repel(data = potential_outliers, aes(label = model)) +
+  geom_point(data = potential_outliers, color = "red") +
+  geom_point(
+    data = potential_outliers,
+    color = "red", size = 3, shape = "circle open"
+  )
 
+trend_text <- "Larger engine sizes tent to\nhave lower fuel economy." %>% 
+  str_wrap(width = 30)
 
+trend_text
 
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point() +
+  annotate(
+    geom = "label", x = 3.5, y = 38,
+    label = trend_text,
+    hjust = "left", color = "red"
+  ) +
+  annotate(
+    geom = "segment",
+    x = 3, y = 35, xend = 5, yend = 25, color = "red",
+    arrow = arrow(type = "closed")
+  )
 
-
-
-
+### Scales
 
 
 
