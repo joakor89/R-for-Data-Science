ad864b994e7fe42558949e26bb945b2aeeddf974
joakor89 <joacorf33@gmail.com>
2025-10-19 08:02:37 -0600

    Comparisons

--- chap_12/12_LV.R
+++ chap_12/12_LV.R
@@ -4,3 +4,126 @@
 
 library(tidyverse)
 library(nycflights13)
+
+x <- c(1, 2, 3, 5, 7, 11, 13)
+
+x * 2
+
+df <- tibble(x)
+
+df %>% 
+  mutate(y = x * 2)
+
+# Comparisons
+
+flights %>% 
+  filter(dep_time > 600 & dep_time < 2000 & abs(arr_delay) < 20)
+
+flights %>% 
+  mutate(
+    daytime = dep_time > 600 & dep_time < 2000,
+    approx_time = abs(arr_delay) < 20,
+    .keep = "used"
+  )
+
+flights %>% 
+  mutate(
+    daytime = dep_time > 600 & dep_time < 2000,
+    approx_ontime = abs(arr_delay) < 20,
+  ) %>% 
+  filter(daytime & approx_ontime)
+
+####
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
