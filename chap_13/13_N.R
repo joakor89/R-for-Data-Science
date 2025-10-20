bda46b57134fd862db5d2dc31147dca9f5d5c920
joakor89 <joacorf33@gmail.com>
2025-10-20 08:01:43 -0600

    Counts

--- chap_13/13_N.R
+++ chap_13/13_N.R
@@ -13,14 +13,35 @@ parse_double(x)
 x <- c("$1.234", "USD 3,513", "%59%")
 parse_number(x)
 
+### Counts
 
+flights %>% count(dest)
 
+flights %>% count(dest, sort = TRUE)
 
+flights %>% 
+  group_by(dest) %>% 
+  summarize(
+    n = n(),
+    delay = mean(arr_delay, na.rm = TRUE)
+  )
 
+n()
 
+flights %>% 
+  group_by(dest) %>% 
+  summarize(carriers = n_distinct(carrier)) %>% 
+  arrange(desc(carriers))
 
+flights %>% 
+  group_by(tailnum) %>% 
+  summarize(miles = sum(distance))
 
+flights %>%  count(tailnum, wt = distance)
 
+flights %>% 
+  group_by(dest) %>% 
+  summarize(n_cancelled = sum(is.na(dep_time)))
 
 
 
