948f7c6da8306c4fce6d0d45e5ba7ee6c11d42f4
joakor89 <joacorf33@gmail.com>
2025-10-21 09:49:54 -0600

    Creating a String

--- chap_14/14_S.R
+++ chap_14/14_S.R
@@ -5,6 +5,10 @@
 library(tidyverse)
 library(babynames)
 
+### Creating a String
 
+string1 <- "This is a string"
 
+string2 <- 'If I want to include a "quote" inside a string, I use single quotes'
 
+#### Escapes
