2bc4b3e922cc0c1df03354afd54235a1db099a51
joakor89 <joacorf33@gmail.com>
2025-10-21 09:53:20 -0600

    Escapes

--- chap_14/14_S.R
+++ chap_14/14_S.R
@@ -12,3 +12,15 @@ string1 <- "This is a string"
 string2 <- 'If I want to include a "quote" inside a string, I use single quotes'
 
 #### Escapes
+
+double_quote <- "\"" # or '"'
+
+single_quote <- '\'' # or "'"
+
+backslash <- "\\"
+
+x <- c(single_quote, double_quote, backslash)
+x
+
+str_view(x)
+
