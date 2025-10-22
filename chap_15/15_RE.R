0c8f92e57087ba34c2681107b6e70be3f1ae5d51
joakor89 <joacorf33@gmail.com>
2025-10-22 09:56:46 -0600

    Pre-requisites: Pattern Basics

--- chap_15/15_RE.R
+++ chap_15/15_RE.R
@@ -6,3 +6,28 @@ library(tidyverse)
 library(babynames)
 
 ### Pattern Basics
+
+str_view(fruit, "berry")
+
+str_view(c("a", "ab", "ae", "bd", "ea", "eab"), "a.")
+
+str_view(fruit, "a...e")
+
+str_view(c("a", "ab", "abb"), "ab?")
+
+str_view(c("a", "ab", "abb"), "ab+")
+
+str_view(c(c("a", "ab", "abb"), "ab*"))
+
+str_view(words, "[aeiou]x[aeiou]")
+
+str_view(words, "[^aeiou]y[^aeiou]")
+
+str_view(fruit, "apple|melon|nut")
+
+str_view(fruit, "aa|ee|ii|oo|uu")
+
+### Key Functions
+
+#### Detect Matches
+
