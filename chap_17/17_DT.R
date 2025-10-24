ea97712a0d9f9d5d160869a2bba8116ddd1b1123
joakor89 <joacorf33@gmail.com>
2025-10-24 10:40:40 -0600

    From Individual Components

--- chap_17/17_DT.R
+++ chap_17/17_DT.R
@@ -42,14 +42,41 @@ mdy_hm("01/31/3017 08:01")
 
 ymd("2017-01-31", tz = "UTC")
 
+#### From Individual Components
 
+flights %>% 
+  select(year, month, day, hour, minute)
 
+flights %>% 
+  select(year, month, day, hour, minute) %>% 
+  mutate(departure = make_datetime(year, month, day, hour, minute))
 
+make_datetime_100 <- function(year, month, day, time) {
+  make_datetime(year, month, day, time %/% 100, time %% 100)
+}
 
+flights_dt <- flights %>% 
+  filter(!is.na(dep_time), !is.na(arr_time)) %>% 
+  mutate(
+    dep_time = make_datetime_100(year, month, day, dep_time),
+    arr_time = make_datetime_100(year, month, day, arr_time),
+    sched_dep_time = make_datetime_100(year, month, day, sched_dep_time),
+    sched_arr_time = make_datetime_100(year, month, day, sched_arr_time)
+  ) %>% 
+  select(origin, dest, ends_with("delay"), ends_with("time"))
 
+flights_dt
 
+flights_dt %>% 
+  ggplot(aes(x = dep_time)) +
+  geom_freqpoly(binwidth = 86400)
 
+flights_dt %>% 
+  filter(dep_time < ymd(20130102)) %>% 
+  ggplot(aes(x = dep_time)) +
+  geom_freqpoly(binwidth = 600)
 
+#### 
 
 
 
