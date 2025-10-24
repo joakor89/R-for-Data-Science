f3515095fc7b7a6b5a0fd7f6d555c77c5683494b
joakor89 <joacorf33@gmail.com>
2025-10-24 09:51:20 -0600

    During Import

--- chap_17/17_DT.R
+++ chap_17/17_DT.R
@@ -10,11 +10,23 @@ today()
 
 now()
 
-#### During 
+#### During Import
 
+csv <- "
+date,datetime
+2022-01-02-01-02 05:12
+"
 
+csv <- "
+date
+01/02/15
+"
 
+read_csv(csv, col_types = cols(date = col_date("%m/%d/%y")))
 
+read_csv(csv, col_types = cols(date = col_date("%d/%m/%y")))
+
+read_csv(csv, col_types = cols(date = col_date("%y/%m/%d")))
 
 
 
