09b29634f16a6116d764664a518c5dec9a3f632b
joakor89 <joacorf33@gmail.com>
2025-10-25 08:05:31 -0600

    Rounding

--- chap_17/17_DT.R
+++ chap_17/17_DT.R
@@ -121,13 +121,34 @@ flights_dt %>%
   ggplot(aes(x = minute, y = avg_delay)) +
   geom_line()
 
+sched_dep <- flights_dt %>% 
+  mutate(minute = minute(sched_dep_time)) %>% 
+  group_by(minute) %>% 
+  summarize(
+    avg_delay = mean(arr_delay, na.rm = TRUE),
+    n = n()
+  )
 
+ggplot(sched_dep, aes(x = minute, y = avg_delay)) +
+  geom_line()
 
+#### Rounding
 
+flights_dt %>% 
+  count(week = floor_date(dep_time, "week")) %>% 
+  ggplot(aes(x = week, y = n)) +
+  geom_line() +
+  geom_point()
 
+flights_dt %>% 
+  mutate(dep_hour = dep_time - floor_date(dep_time, "day")) %>% 
+  ggplot(aes(x = dep_hour)) +
+  geom_freqpoly(binwidth = 60 * 30)
 
-
-
+flights_dt %>% 
+  mutate(dep_hour = hms::as_hms(dep_time - floor_date(dep_time, "day"))) %>% 
+  ggplot(aes(x = dep_hour)) +
+  geom_freqpoly(binwidth = 60 * 30)
 
 
 
