8c7fc748c769f738b48c91e99b65444014ffb8aa
joakor89 <joacorf33@gmail.com>
2025-10-26 15:14:24 -0600

    Keys: Primary & Foreign Keys

--- chap_19/19_J.R
+++ chap_19/19_J.R
@@ -5,16 +5,29 @@
 library(tidyverse)
 library(nycflights13)
 
+### Keys
 
+#### Primary & Foreign Keys
 
+airlines
 
+airports
 
+planes
 
+weather
 
+flights$tailnum
 
+flights$carrier
 
+flights$origin
 
+flights$dest
 
+flights$origin-flights$time_hour
+
+#### 
 
 
 
