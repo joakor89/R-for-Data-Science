c491c8c5ab27181570942c060340b41bd7029e1a
joakor89 <joacorf33@gmail.com>
2025-10-26 12:21:30 -0600

    Explicit Missing Values

--- chap_18/18_MV.R
+++ chap_18/18_MV.R
@@ -5,6 +5,19 @@ library(tidyverse)
 
 ### Explicit Missing Values
 
+treatment <- tribble(
+  ~person,            ~treatment, ~response,
+  "Derrick Whitmore", 1,          7,
+  NA,                 2,          10,
+  NA,                 3,          NA,
+  "Katherine Burke",  1,          4
+)
+
+treatment %>% 
+  fill(everything())
+
+
+#### Fixed Values
 
 
 
