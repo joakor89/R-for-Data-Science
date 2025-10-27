9072a5bcb401e679e08d71d2d149ef373069b14e
joakor89 <joacorf33@gmail.com>
2025-10-27 08:07:35 -0600

    How Do Joins Work?

--- chap_19/19_J.R
+++ chap_19/19_J.R
@@ -112,6 +112,19 @@ flights2 %>%
 
 ### How Do Joins Work?
 
+x <- tribble(
+  ~key, ~val_x,
+  1, "x1",
+  2, "x2",
+  3, "x3"
+)
+
+y <- tribble(
+  ~key, ~val_y,
+  1, "y1", 
+  2, "y2",
+  4, "y3"
+)
 
 
 
