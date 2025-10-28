81b7fb7b67a77047defb06eb74b971d3d113aa51
joakor89 <joacorf33@gmail.com>
2025-10-28 08:45:21 -0600

    Getting Started: Reading Excel Spreadsheets

--- chap_20/20_SS.R
+++ chap_20/20_SS.R
@@ -10,3 +10,155 @@ library(writexl)
 
 #### Reading Excel Spreadsheets
 
+students <- read_excel("/Users/joaquinromero/desktop/R4DS/data/students.xlsx")
+
+students
+
+read_excel(
+  "/Users/joaquinromero/desktop/R4DS/data/students.xlsx",
+  col_names = c(
+    "student_id", "full_name", "favourite_food", "meal_plan", "age")
+)
+
+read_excel(
+  "/Users/joaquinromero/desktop/R4DS/data/students.xlsx",
+  col_names = c(
+    "student_id", "full_name", "favourite_food", "meal_plan", "age"),
+  skip = 1
+)
+
+read_excel(
+  "/Users/joaquinromero/desktop/R4DS/data/students.xlsx",
+  col_names = c(
+    "student_id", "full_name", "favourite_food", "meal_plan", "age"),
+  skip = 1,
+  na = c("", "N/A")
+)
+
+read_excel(
+  "/Users/joaquinromero/desktop/R4DS/data/students.xlsx",
+  col_names = c(
+    "student_id", "full_name", "favourite_food", "meal_plan", "age"),
+  skip = 1,
+  na = c("", "N/A"),
+  col_types = c("numeric", "text", "text", "text", "numeric")
+)
+
+read_excel(
+  "/Users/joaquinromero/desktop/R4DS/data/students.xlsx",
+  col_names = c("student_id", "full_name", "favourite_food", "meal_plan", "age"),
+  skip = 1,
+  na = c("", "N/A"),
+  col_types = c("numeric", "text", "text", "text", "text")
+)
+
+students <- students %>%
+  mutate(age = as.character(age)) %>%                   
+  mutate(age = if_else(age == "five, 5", "5", age)) %>% 
+  mutate(age = parse_number(age))     
+
+students
+
+### Reading Worksheets
+
+write.xlsx(penguins, "/Users/joaquinromero/desktop/R4DS/data/penguins.xlsx")
+penguins
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
