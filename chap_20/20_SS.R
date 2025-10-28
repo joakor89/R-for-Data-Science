f430ad76c86faf3ab5783b4bdc0d9290dab91cb0
joakor89 <joacorf33@gmail.com>
2025-10-28 09:46:03 -0600

    Reading Google Sheets

--- chap_20/20_SS.R
+++ chap_20/20_SS.R
@@ -107,12 +107,34 @@ write_xlsx(bake_sale, path = "data/bake-sale.xlsx")
 
 read_excel("data/bake-sale.xlsx")
 
+### Google Sheets
 
+#### Pre-requisites
+library(googlesheets4)
+library(tidyverse)
 
+#### Reading Google Sheets
 
+students_sheet_id <- "1V1nPp11tzOuuyXFLb3G9Eyxi3qxeEhnOXUzL5_BcCQ0w"
+students <- read_sheet(students_sheet_id)
 
+students
+students <- read_sheet(
+  students_sheet_id,
+  col_names = c("student_id", "full_name", "favourite_food", "meal_plan", "age"),
+  ski = 1,
+  na = c("", "N/A"),
+  col_types = "dcccc"
+)
 
+penguis_sheet_id <- "1aFu8lnD_g0yjF50-k6SFgSEWiHPpgvFCF0NY9D6LXnY"
+read_sheet(penguis_sheet_id, sheet = "Torgesen Island")
 
+sheet_names(penguis_sheet_id)
+
+deaths_url <- gs4_example("death")
+deaths <-read_sheet(deaths_url, range = "A5:F15")
+deaths
 
 
 
